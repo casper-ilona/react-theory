6c7b7e36eae0cba1fee835fc1ed7b298f813d66b
Ilona <hair.masterpiece@gmail.com>
2020-03-17 16:47:39 +0300

    Add .idea to git ignore list.сделала прикольный эффект при навидение стрелочки на обьект он выделяется

--- src/App.js
+++ src/App.js
@@ -10,67 +10,71 @@ class App extends Component {
             {name: 'Audi', year: 2016},
             {name: 'Mazda', year: 2010},
         ],
-        pageTitle: 'React components'
+        pageTitle: 'React components',
+        showCars: false
     };
     
-    changeTitleHandler = (newTitle) => {
+    toggleCarsHandler = () => {
         this.setState({
-            pageTitle: newTitle
+            showCars: !this.state.showCars
         });
     };
     
-    handleInput = (event) => {
+    onChangeName(name, index) {
+        const car = this.state.cars[index];
+        car.name = name;
+        const cars = [...this.state.cars];
+        cars [index] = car;
         this.setState({
-            pageTitle: event.target.value
+            cars
         });
+    }
+    
+    deleteHandler(index) {
+        const cars = this.state.cars.concat();
+        cars.splice(index, 1);
+        
+        this.setState({cars});
     };
     
     render() {
         console.log('Render');
         const divStyle = {
             textAlign: 'center'
+        };
+        
+        let cars = null;
+        
+        if (this.state.showCars) {
+            cars = this.state.cars.map((car, index) => {
+                return (
+                    <Car
+                        key={index}
+                        name={car.name}
+                        year={car.year}
+                        onDelete={this.deleteHandler.bind(this, index)}
+                        onChangeName={event => this.onChangeName(event.target.value, index)}
+                    />
+                
+                );
+            });
         }
+        
         return (
             <div style={divStyle}>
                 <h1>{this.state.pageTitle}</h1>
                 
-                <input type="text" onChange={this.handleInput}/>
-                
                 <button
-                    onClick={this.changeTitleHandler.bind(this, 'Changed!')}
-                >Change title
+                    onClick={this.toggleCarsHandler}
+                >Toggle cars
                 </button>
-                
-                {this.state.cars.map((car) => {
-                    return (
-                        <Car
-                            key={`car-${car.name}`}
-                            name={car.name}
-                            year={car.year}
-                            onChangeTitle={() => this.changeTitleHandler(car.name)}
-                        />
-                    )
-                })}
-                
-                
-                {/*<Car*/}
-                {/*    name={cars[0].name}*/}
-                {/*    year={cars[0].year}*/}
-                {/*    onChangeTitle={this.changeTitleHandler.bind(this, cars[0].name)}*/}
-                
-                
-                {/*/>*/}
-                {/*<Car*/}
-                {/*    name={cars[1].name}*/}
-                {/*    year={cars[1].year}*/}
-                {/*    onChangeTitle={() => this.changeTitleHandler(cars[1].name)}*/}
-                
-                {/*/>*/}
-                {/*<Car*/}
-                {/*    name={cars[2].name}*/}
-                {/*    year={cars[2].year}*/}
-                {/*    onChangeTitle={() => this.changeTitleHandler(cars[2].name)}*/}
-                {/*/>*/}
+                <div style={{
+                  width:400,
+                    margin: 'auto',
+                    paddingTop:'20px'
+                }}>
+                {cars}
+                </div>
             </div>
         );
         //      return React.createElement(
