9776ed09d0acbd7c64f8fd4eccd4a97e75a3124e
Ilona <hair.masterpiece@gmail.com>
2020-04-14 17:46:43 +0300

    Навигация между страницами

--- src/App.js
+++ src/App.js
@@ -1,38 +1,43 @@
-import React, {Component} from 'react'
-import './App.scss'
-import About from './About/About'
-import Cars from './Cars/Cars'
-import {Route} from 'react-router-dom'
+import React, { Component } from 'react';
+import './App.scss';
+import About from './About/About';
+import Cars from './Cars/Cars';
+import { Route } from 'react-router-dom';
 
 class App extends Component {
-  render() {
-
-    return (
-      <div>
-        <nav className="nav">
-          <ul>
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="/about">About</a>
-            </li>
-          </ul>
-        </nav>
-
-        <hr/>
+    render() {
         
-        
-        
-          {/*lokalhost:3000,*/}
-        < Route path="/" exact render={()=><h1>Home Page</h1>} />
-        
-        <About />
-
-        <Cars />
-      </div>
-    );
-  }
+        return (
+            <div>
+                <nav className="nav">
+                    <ul>
+                        <li>
+                            <a href="/">Home</a>
+                        </li>
+                        <li>
+                            <a href="/about">About</a>
+                        </li>
+                        
+                        <li>
+                            <a href="/cars">Cars</a>
+                        </li>
+                    </ul>
+                </nav>
+                
+                <hr/>
+                
+                
+                {/*lokalhost:3000,*/}
+                < Route path="/" exact render={() => <h1>Home Page</h1>}/>
+                <Route path="/about" component={About}/>
+                <Route path="/cars" component={Cars}/>
+                
+                {/*<About />*/}
+                
+                {/*<Cars />*/}
+            </div>
+        );
+    }
 }
 
- export default App
+export default App;
